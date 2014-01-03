e9fd0c8d26b834948e6ec64e1828e752be3f6f8f
FSGMgmnt <fsgmgmnt@fsgin.firstswissgroup.com>
2014-01-03 10:39:44 -0500

    Up dated chart definition

--- UI/specs/sampleLineChart2.js
+++ UI/specs/sampleLineChart2.js
@@ -1,27 +1,723 @@
 {
-	id: 1,
-	sourceID: 1,
-	title: "Widget title",
-	description: "Widget Description",
-	position:{
-		row:1,
-		col:1
-	},
-	size:{
-		width:1,
-		height:1
-	}
-	spec: {
-		'name': 'metric1',
-		'styles': [
-		],
-		'scales': [
-		],
-		'axes': [
-		],
-		'legends': [
-		],
-		'marks': [
-		]
-	}
+  "width": 350,
+  "height": 200,
+  "padding": {
+    "top": 10,
+    "left": 50,
+    "bottom": 60,
+    "right": 50
+  },
+  "data": [
+    {
+      "name": "table",
+      "format": {
+        "type": "json",
+        "parse": {
+          "Date": "date",
+          "Canada": "number",
+          "US": "number"
+        }
+      },
+      "values": [
+        {
+          "Date": "01/01/2006",
+          "Canada": "6.60",
+          "US": "4.70"
+        },
+        {
+          "Date": "02/01/2006",
+          "Canada": "6.40",
+          "US": "4.80"
+        },
+        {
+          "Date": "03/01/2006",
+          "Canada": "6.30",
+          "US": "4.70"
+        },
+        {
+          "Date": "04/01/2006",
+          "Canada": "6.30",
+          "US": "4.70"
+        },
+        {
+          "Date": "05/01/2006",
+          "Canada": "6.00",
+          "US": "4.60"
+        },
+        {
+          "Date": "06/01/2006",
+          "Canada": "6.10",
+          "US": "4.60"
+        },
+        {
+          "Date": "07/01/2006",
+          "Canada": "6.30",
+          "US": "4.70"
+        },
+        {
+          "Date": "08/01/2006",
+          "Canada": "6.40",
+          "US": "4.70"
+        },
+        {
+          "Date": "09/01/2006",
+          "Canada": "6.40",
+          "US": "4.50"
+        },
+        {
+          "Date": "10/01/2006",
+          "Canada": "6.20",
+          "US": "4.40"
+        },
+        {
+          "Date": "11/01/2006",
+          "Canada": "6.30",
+          "US": "4.50"
+        },
+        {
+          "Date": "12/01/2006",
+          "Canada": "6.10",
+          "US": "4.40"
+        },
+        {
+          "Date": "01/01/2007",
+          "Canada": "6.20",
+          "US": "4.60"
+        },
+        {
+          "Date": "02/01/2007",
+          "Canada": "6.20",
+          "US": "4.50"
+        },
+        {
+          "Date": "03/01/2007",
+          "Canada": "6.10",
+          "US": "4.40"
+        },
+        {
+          "Date": "04/01/2007",
+          "Canada": "6.10",
+          "US": "4.50"
+        },
+        {
+          "Date": "05/01/2007",
+          "Canada": "6.00",
+          "US": "4.40"
+        },
+        {
+          "Date": "06/01/2007",
+          "Canada": "6.00",
+          "US": "4.60"
+        },
+        {
+          "Date": "07/01/2007",
+          "Canada": "6.10",
+          "US": "4.70"
+        },
+        {
+          "Date": "08/01/2007",
+          "Canada": "6.00",
+          "US": "4.60"
+        },
+        {
+          "Date": "09/01/2007",
+          "Canada": "5.90",
+          "US": "4.70"
+        },
+        {
+          "Date": "10/01/2007",
+          "Canada": "5.90",
+          "US": "4.70"
+        },
+        {
+          "Date": "11/01/2007",
+          "Canada": "6.00",
+          "US": "4.70"
+        },
+        {
+          "Date": "12/01/2007",
+          "Canada": "6.00",
+          "US": "5.00"
+        },
+        {
+          "Date": "01/01/2008",
+          "Canada": "5.90",
+          "US": "5.00"
+        },
+        {
+          "Date": "02/01/2008",
+          "Canada": "5.90",
+          "US": "4.80"
+        },
+        {
+          "Date": "03/01/2008",
+          "Canada": "6.00",
+          "US": "5.10"
+        },
+        {
+          "Date": "04/01/2008",
+          "Canada": "6.10",
+          "US": "4.90"
+        },
+        {
+          "Date": "05/01/2008",
+          "Canada": "6.10",
+          "US": "5.40"
+        },
+        {
+          "Date": "06/01/2008",
+          "Canada": "6.00",
+          "US": "5.60"
+        },
+        {
+          "Date": "07/01/2008",
+          "Canada": "6.10",
+          "US": "5.80"
+        },
+        {
+          "Date": "08/01/2008",
+          "Canada": "6.10",
+          "US": "6.10"
+        },
+        {
+          "Date": "09/01/2008",
+          "Canada": "6.20",
+          "US": "6.20"
+        },
+        {
+          "Date": "10/01/2008",
+          "Canada": "6.20",
+          "US": "6.60"
+        },
+        {
+          "Date": "11/01/2008",
+          "Canada": "6.50",
+          "US": "6.80"
+        },
+        {
+          "Date": "12/01/2008",
+          "Canada": "6.80",
+          "US": "7.30"
+        },
+        {
+          "Date": "01/01/2009",
+          "Canada": "7.30",
+          "US": "7.80"
+        },
+        {
+          "Date": "02/01/2009",
+          "Canada": "8.00",
+          "US": "8.20"
+        },
+        {
+          "Date": "03/01/2009",
+          "Canada": "8.10",
+          "US": "8.60"
+        },
+        {
+          "Date": "04/01/2009",
+          "Canada": "8.10",
+          "US": "8.90"
+        },
+        {
+          "Date": "05/01/2009",
+          "Canada": "8.50",
+          "US": "9.40"
+        },
+        {
+          "Date": "06/01/2009",
+          "Canada": "8.60",
+          "US": "9.50"
+        },
+        {
+          "Date": "07/01/2009",
+          "Canada": "8.60",
+          "US": "9.50"
+        },
+        {
+          "Date": "08/01/2009",
+          "Canada": "8.70",
+          "US": "9.70"
+        },
+        {
+          "Date": "09/01/2009",
+          "Canada": "8.30",
+          "US": "9.80"
+        },
+        {
+          "Date": "10/01/2009",
+          "Canada": "8.40",
+          "US": "10.10"
+        },
+        {
+          "Date": "11/01/2009",
+          "Canada": "8.40",
+          "US": "9.90"
+        },
+        {
+          "Date": "12/01/2009",
+          "Canada": "8.40",
+          "US": "9.90"
+        },
+        {
+          "Date": "01/01/2010",
+          "Canada": "8.30",
+          "US": "9.70"
+        },
+        {
+          "Date": "02/01/2010",
+          "Canada": "8.20",
+          "US": "9.70"
+        },
+        {
+          "Date": "03/01/2010",
+          "Canada": "8.20",
+          "US": "9.70"
+        },
+        {
+          "Date": "04/01/2010",
+          "Canada": "8.10",
+          "US": "9.80"
+        },
+        {
+          "Date": "05/01/2010",
+          "Canada": "8.10",
+          "US": "9.60"
+        },
+        {
+          "Date": "06/01/2010",
+          "Canada": "7.90",
+          "US": "9.50"
+        },
+        {
+          "Date": "07/01/2010",
+          "Canada": "8.00",
+          "US": "9.50"
+        },
+        {
+          "Date": "08/01/2010",
+          "Canada": "8.10",
+          "US": "9.60"
+        },
+        {
+          "Date": "09/01/2010",
+          "Canada": "8.00",
+          "US": "9.60"
+        },
+        {
+          "Date": "10/01/2010",
+          "Canada": "7.90",
+          "US": "9.70"
+        },
+        {
+          "Date": "11/01/2010",
+          "Canada": "7.60",
+          "US": "9.80"
+        },
+        {
+          "Date": "12/01/2010",
+          "Canada": "7.60",
+          "US": "9.40"
+        },
+        {
+          "Date": "01/01/2011",
+          "Canada": "7.80",
+          "US": "9.00"
+        },
+        {
+          "Date": "02/01/2011",
+          "Canada": "7.80",
+          "US": "8.90"
+        },
+        {
+          "Date": "03/01/2011",
+          "Canada": "7.70",
+          "US": "8.80"
+        },
+        {
+          "Date": "04/01/2011",
+          "Canada": "7.60",
+          "US": "9.00"
+        },
+        {
+          "Date": "05/01/2011",
+          "Canada": "7.40",
+          "US": "9.10"
+        },
+        {
+          "Date": "06/01/2011",
+          "Canada": "7.40",
+          "US": "9.20"
+        },
+        {
+          "Date": "07/01/2011",
+          "Canada": "7.20",
+          "US": "9.10"
+        },
+        {
+          "Date": "08/01/2011",
+          "Canada": "7.30",
+          "US": "9.10"
+        },
+        {
+          "Date": "09/01/2011",
+          "Canada": "7.10",
+          "US": "9.10"
+        },
+        {
+          "Date": "10/01/2011",
+          "Canada": "7.30",
+          "US": "9.00"
+        },
+        {
+          "Date": "11/01/2011",
+          "Canada": "7.40",
+          "US": "8.60"
+        },
+        {
+          "Date": "12/01/2011",
+          "Canada": "7.50",
+          "US": "8.50"
+        },
+        {
+          "Date": "01/01/2012",
+          "Canada": "7.60",
+          "US": "8.30"
+        },
+        {
+          "Date": "02/01/2012",
+          "Canada": "7.40",
+          "US": "8.30"
+        },
+        {
+          "Date": "03/01/2012",
+          "Canada": "7.20",
+          "US": "8.20"
+        },
+        {
+          "Date": "04/01/2012",
+          "Canada": "7.30",
+          "US": "8.10"
+        },
+        {
+          "Date": "05/01/2012",
+          "Canada": "7.30",
+          "US": "8.20"
+        },
+        {
+          "Date": "06/01/2012",
+          "Canada": "7.20",
+          "US": "8.20"
+        },
+        {
+          "Date": "07/01/2012",
+          "Canada": "7.30",
+          "US": "8.30"
+        },
+        {
+          "Date": "08/01/2012",
+          "Canada": "7.30",
+          "US": "8.10"
+        },
+        {
+          "Date": "09/01/2012",
+          "Canada": "7.40",
+          "US": "7.80"
+        },
+        {
+          "Date": "10/01/2012",
+          "Canada": "7.40",
+          "US": "7.90"
+        },
+        {
+          "Date": "11/01/2012",
+          "Canada": "7.20",
+          "US": "7.80"
+        },
+        {
+          "Date": "12/01/2012",
+          "Canada": "7.10",
+          "US": "7.80"
+        },
+        {
+          "Date": "01/01/2013",
+          "Canada": "7.00",
+          "US": "7.90"
+        },
+        {
+          "Date": "02/01/2013",
+          "Canada": "7.00",
+          "US": "7.70"
+        },
+        {
+          "Date": "03/01/2013",
+          "Canada": "7.20",
+          "US": "7.60"
+        },
+        {
+          "Date": "04/01/2013",
+          "Canada": "7.20",
+          "US": "7.50"
+        },
+        {
+          "Date": "05/01/2013",
+          "Canada": "7.10",
+          "US": "7.60"
+        },
+        {
+          "Date": "06/01/2013",
+          "Canada": "7.10",
+          "US": "7.60"
+        },
+        {
+          "Date": "07/01/2013",
+          "Canada": "7.20",
+          "US": "7.40"
+        },
+        {
+          "Date": "08/01/2013",
+          "Canada": "7.10",
+          "US": "7.30"
+        },
+        {
+          "Date": "09/01/2013",
+          "Canada": "6.90",
+          "US": "7.20"
+        }
+      ]
+    }
+  ],
+  "scales": [
+    {
+      "name": "x",
+      "type": "time",
+      "range": "width",
+      "nice": "week",
+      "domain": {
+        "data": "table",
+        "field": "data.Date"
+      }
+    },
+    {
+      "name": "y",
+      "type": "linear",
+      "range": "height",
+      "zero": false,
+      "nice": true,
+      "domain": {
+        "data": "table",
+        "field": [
+          "data.Canada",
+          "data.US"
+        ]
+      }
+    }
+  ],
+  "axes": [
+    {
+      "type": "x",
+      "scale": "x",
+      "grid": true,
+      "title": "Dates",
+      "titleOffset": 40,
+      "layer": "back",
+      "properties": {
+        "title": {
+          "font": {
+            "value": "Segoe UI"
+          },
+          "fontSize": {
+            "value": 12
+          },
+          "fontWeight": {
+            "value": "bold"
+          }
+        },
+        "labels": {
+          "font": {
+            "value": "Segoe UI"
+          },
+          "fontSize": {
+            "value": 10
+          },
+          "dx": {
+            "value": 10
+          },
+          "angle": {
+            "value": 25
+          }
+        }
+      }
+    },
+    {
+      "type": "y",
+      "scale": "y",
+      "grid": true,
+      "title": "%",
+      "layer": "back",
+      "properties": {
+        "title": {
+          "font": {
+            "value": "Segoe UI"
+          },
+          "fontSize": {
+            "value": 12
+          },
+          "fontWeight": {
+            "value": "bold"
+          }
+        },
+        "labels": {
+          "font": {
+            "value": "Segoe UI"
+          },
+          "fontSize": {
+            "value": 10
+          }
+        }
+      }
+    }
+  ],
+  "marks": [
+    {
+      "type": "line",
+      "from": {
+        "data": "table"
+      },
+      "properties": {
+        "enter": {
+          "interpolate": {
+            "value": "linear"
+          },
+          "x": {
+            "scale": "x",
+            "field": "data.Date"
+          },
+          "y": {
+            "scale": "y",
+            "field": "data.Canada"
+          },
+          "stroke": {
+            "value": "#0000cc"
+          },
+          "strokeWidth": {
+            "value": 2
+          }
+        }
+      }
+    },
+    {
+      "type": "line",
+      "from": {
+        "data": "table"
+      },
+      "properties": {
+        "enter": {
+          "interpolate": {
+            "value": "linear"
+          },
+          "x": {
+            "scale": "x",
+            "field": "data.Date"
+          },
+          "y": {
+            "scale": "y",
+            "field": "data.US"
+          },
+          "stroke": {
+            "value": "#cc0000"
+          },
+          "strokeWidth": {
+            "value": 2
+          }
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {
+        "data": "table",
+        "transform": [
+          {
+            "type": "filter",
+            "test": "index==data.length-1"
+          }
+        ]
+      },
+      "properties": {
+        "enter": {
+          "x": {
+            "scale": "x",
+            "field": "data.Date",
+            "offset": 2
+          },
+          "y": {
+            "scale": "y",
+            "field": "data.US"
+          },
+          "fill": {
+            "value": "#cc0000"
+          },
+          "text": {
+            "value": "US"
+          },
+          "baseline": {
+            "value": "middle"
+          },
+          "font": {
+            "value": "Segoe UI"
+          },
+          "fontSize": {
+            "value": 12
+          },
+          "fontWeight": {
+            "value": "bold"
+          }
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {
+        "data": "table",
+        "transform": [
+          {
+            "type": "filter",
+            "test": "index==data.length-1"
+          }
+        ]
+      },
+      "properties": {
+        "enter": {
+          "x": {
+            "scale": "x",
+            "field": "data.Date",
+            "offset": 2
+          },
+          "y": {
+            "scale": "y",
+            "field": "data.Canada"
+          },
+          "fill": {
+            "value": "#0000cc"
+          },
+          "text": {
+            "value": "Canada"
+          },
+          "baseline": {
+            "value": "middle"
+          },
+          "font": {
+            "value": "Segoe UI"
+          },
+          "fontSize": {
+            "value": 12
+          },
+          "fontWeight": {
+            "value": "bold"
+          }
+        }
+      }
+    }
+  ]
 }
