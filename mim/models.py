abd4294d3359825aa9b29d7190a85b1c8551d349
sacha schutz <sacha@MacBook-Pro-de-Perrine.local>
2014-01-08 18:05:32 +0100

    add models for mim

---
+++ mim/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class View(models.Model):
+	title = models.CharField(max_length=64, default="No Title")
+	description = models.CharField(max_length=256, default="No Description")
+	style = models.CharField(max_length=32, default="default")
+	user = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.title
+
+
+class Widget(models.Model):
+	title = models.CharField(max_length=64, default="No Title")
+	description = models.CharField(max_length=256, default="No Description")
+	row = models.IntegerField(default=0)
+	col = models.IntegerField(default=0)
+	width = models.IntegerField(default=380)
+	height = models.IntegerField(default=300)
+	spec = models.TextField()
+	view = models.ForeignKey(View)
+
+	def __unicode__(self):
+		return self.title
+
