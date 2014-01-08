6a42871b9f45808e1ddaa6589568281e8373b725
FSGMgmnt <fsgmgmnt@fsgin.firstswissgroup.com>
2014-01-08 12:47:33 -0500

    Vega wrapper

--- UI/public/javascripts/mimui.vega.js
+++ UI/public/javascripts/mimui.vega.js
@@ -4,15 +4,17 @@
 if(mimUI!=undefined){
     mimUI.vega = {
         _spec: 0,
-        _specSize: {width:0,height:0},
-        
+        _widgetSpan: {rows:0,cols:0},
+        _widgetSize: {width:0,height:0},
+        _widgetMargin: {top:0,right:0,bottom:0,left:0},
+        _viewMargin: {top:0,right:0,bottom:0,left:0},
         setSpec: function(spec){
             mimUI.vega._spec = spec;
         },
         getSpec: function(){
             return mimUI.vega._spec;
         },
-        comleteSpec: function(spec){
+        prepareSpec: function(spec){
             //TODO we finalise spec here before passing it to vega parser to make sure it's complete
             return spec;
         }
