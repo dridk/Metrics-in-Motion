051e4f449d57019dd2a41113e3cc7edffa1c8cae
Sacha schutz <sacha@labsquare.org>
2014-06-29 14:25:18 +0200

    now it uses ObjectId and parse it to get id field and createdField

--- server/api/common.py
+++ server/api/common.py
@@ -4,13 +4,28 @@ from pymongo import cursor
 from bson import json_util
 import json
 
+''' MongoDB return _id as ObjectID. Which is not parsable as json
+Those methods convert ObjectID to stringID and also date. Indeed, ObjectID contains 
+also the created date ''' 
+
+''' This function parse a dict python '''
+def parseObjectId(data):
+	if "_id" in data:
+		data["created"] = data["_id"].generation_time
+		data["id"] = str(data["_id"])
+		del data["_id"]
+	return data
+
+''' This function parse a list of dict ''' 
+def parseObjectIds(data):
+	for i in range(len(data)):
+		data[i] = parseObjectId(data[i])
+	return data
+
+
+
 
 def SuccessResponse(data = None):
-
-	if type(data) == cursor.Cursor:
-		data = json.loads(json_util.dumps(data))
-
-
 	if data is None:
 		results = {"success":True}
 	else:
--- server/api/users.py
+++ server/api/users.py
@@ -4,6 +4,7 @@ from flask import *
 from common import *
 from model import User
 from bson import json_util
+from bson.objectid import ObjectId
 import json
 resource = Blueprint("user_api", __name__)
 
@@ -11,23 +12,22 @@ resource = Blueprint("user_api", __name__)
 """
 @resource.route("/users", methods=["GET"])
 def users():
-	data = current_app.db.users.find({}, {"views":0, "_id":0})
-	return SuccessResponse(data)
+	data = current_app.db.users.find({}, {"views.widgets":0,"password":0})
+	return SuccessResponse(parseObjectIds(list(data)))
 
 """Search Users from keywords
 """
 @resource.route("/users/search/<string:keyword>", methods=["GET"])
 def search(keyword):
 	data = current_app.db.users.find({"email":{"$regex":'^'+keyword}}, {"_id":True, "email":True})
-	return SuccessResponse(list(data))
+	return SuccessResponse(parseObjectIds(list(data)))
 
 """Search unique user from id 
 """
-@resource.route("/users/<int:user_id>", methods=["GET"])
+@resource.route("/users/<string:user_id>", methods=["GET"])
 def get_user(user_id):
-	data = current_app.db.users.find_one({"_id":user_id}, {"email":True})
-	print(data["email"])
-	return SuccessResponse(data)
+	data = current_app.db.users.find_one({"_id":ObjectId(user_id)}, {"email":True})
+	return SuccessResponse(parseObjectId(data))
 
 """Create user
 """
@@ -41,8 +41,8 @@ def post_user():
 	except Exception, e:
 		return ErrorResponse(e.message,600)
 	else:
-		current_app.db.users.insert(user.to_primitive())
-		return "yes"
+		a = current_app.db.users.insert(user.to_primitive())
+		return SuccessResponse({"id":str(a)})
 
 
 	
