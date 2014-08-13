b1a7ce4a2012a66b1870ee77b2b46ca0c2afa7eb
Sacha schutz <sacha@labsquare.org>
2014-08-13 18:35:32 +0200

    Create dashview models !

--- server/mim/models.py
+++ server/mim/models.py
@@ -1,7 +1,32 @@
 from mongoengine import *
+from datetime import datetime
 class User(Document):
-	email    = EmailField(required=True)
-	nickname = StringField(required=True, max_length=50)
-	password = StringField(required=True, max_length=256) 
+	email       = EmailField(required=True)
+	nickname    = StringField(required=True, max_length=50)
+	password    = StringField(required=True, max_length=255) 
+
+
+class Comment(EmbeddedDocument):
+	owner 		= ReferenceField(User)
+	created 	= DateTimeField(default=datetime.now)
+	comment 	= StringField(max_length=255)	
+
+
+class Vega(DynamicEmbeddedDocument):
+	pass
+
+class Widget(EmbeddedDocument):
+	title  		= StringField(default ="No Title", max_length=50)
+	description = StringField(max_length=255)	
+	comments    = ListField(EmbeddedDocumentField(Comment))
+	vega        = EmbeddedDocumentField(Vega)
+
+
+class DashView(Document):
+	owner 		= ReferenceField(User)
+	title  		= StringField(default ="No Title", max_length=50)
+	description = StringField(max_length=255)	
+	created 	= DateTimeField(default=datetime.now)
+	widgets 	= ListField(EmbeddedDocumentField(Widget))
 
 
