222dcaf07f91e9deef7619acdc0c6cd5ce3315aa
Sacha schutz <sacha@labsquare.org>
2014-08-23 00:13:20 +0200

    add Dashview get API request

--- server/mim/api/dashview_api.py
+++ server/mim/api/dashview_api.py
@@ -4,10 +4,11 @@ from mim.models import *
 from mim.ext import *
 dashview_api = Blueprint("dashview_api", __name__)
 
-
-@dashview_api.route("/dashviews", methods = ["GET"])
-def list():
-#Count how many widgets they are are
+''' This is an extra function, which add "widget_count" key into all 
+dashview results collections. 
+''' 
+def add_widget_count(collection):
+	#Create a dictionnary [dashview_id] = count
 	ids = {}
 	for widget in Widget.objects.all():
 		key = str(widget.dashview.id)
@@ -16,15 +17,19 @@ def list():
  		else:
  			ids[key]+=1
 
-#Get the dashviews list
-	dashviews = toDict(DashView.objects.all()) 
-#Loop over dashviews and add widget_count key / value
-	for index in range(len(dashviews)):
-		key = dashviews[index]["id"]
+#Loop over the collection and add widget_count key and value
+	for index in range(len(collection)):
+		key = collection[index]["id"]
 		print key
 		if key in ids:
-			dashviews[index]["widgets_count"] = ids[key]
+			collection[index]["widgets_count"] = ids[key]
+	return collection
 
+
+@dashview_api.route("/dashviews", methods = ["GET"])
+def list():
+	dashviews = toDict(DashView.objects.all()) 
+	dashviews = add_widget_count(dashviews)
 	return SuccessResponse(dashviews)
 
 
@@ -39,3 +44,18 @@ def create():
 		return ErrorResponse(e.message,600)
 	else: 
 		SuccessResponse(json.dumps({"id":str(user.pk)}))
+
+
+""" Get a User
+"""		
+@dashview_api.route("/dashviews/<string:dashview_id>", methods = ["GET"])
+def get(dashview_id):
+	try:
+		dashview = DashView.objects.get(id=dashview_id)
+	except Exception,e:
+		return ErrorResponse(e.message, 600)
+	else:
+		return SuccessResponse(toDict(dashview))
+
+	
+
