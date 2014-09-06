8e97f24f394407f2c2f45533bf5927f260e5dbf9
Sacha schutz <sacha@labsquare.org>
2014-09-06 12:59:51 +0200

    type pyrg run_test.py -v to verbose !!

--- server/run_test.py
+++ server/run_test.py
@@ -8,9 +8,5 @@ from test.dashview_test import *
 
 
 if __name__ == '__main__':
-	testSuite4User = unittest.TestLoader().loadTestsFromTestCase(UserTest)
-	unittest.TextTestRunner(verbosity=2).run(testSuite4User)
-	
-	testSuite4Dashview = unittest.TestLoader().loadTestsFromTestCase(DashViewTest)
-	unittest.TextTestRunner(verbosity=2).run(testSuite4Dashview)
+	unittest.main()
 
